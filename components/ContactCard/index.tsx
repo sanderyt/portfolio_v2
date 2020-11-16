5524408d2cd7a513551aacba652722b7bd901be2
sanderyt <hallo@svanrijsoort.nl>
2020-11-16 18:14:48 +0000

    added styled components

--- components/ContactCard/index.tsx
+++ components/ContactCard/index.tsx
@@ -1,17 +1,59 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone, faEnvelope, faHome } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
 
 export const ContactCard = ({ icon, text, type }) => {
+  const ContactCard = styled.div`
+    display: flex;
+    width: 400px;
+    cursor: pointer;
+    margin-top: 25px;
+    height: 75px;
+    animation: fadeIn 1s forwards;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 0 10px #aaa;
+    border-radius: 10px;
+    transition: 500ms;
+  `;
+
+  const ContactCardIcon = styled.div`
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    & .fa-phone,
+    .fa-home {
+      color: red;
+      font-size: 1.5rem;
+    }
+  `;
+
+  const ContactCardText = styled.div`
+    background-color: #f7f7f7;
+    border-bottom-right-radius: 5px;
+    align-items: center;
+    border-top-right-radius: 5px;
+    height: 100%;
+    display: flex;
+    flex: 3;
+    justify-content: center;
+    flex-direction: column;
+    & .type {
+      color: #adadad;
+      margin-bottom: 5px;
+    }
+  `;
+
   return (
-    <div className="contact-card">
-      <div className="contact-card__icon">
+    <ContactCard>
+      <ContactCardIcon>
         <FontAwesomeIcon icon={faHome} />
-      </div>
-      <div className="contact-card__text">
+      </ContactCardIcon>
+      <ContactCardText>
         <span className="type">{type}</span>
         <span>{text}</span>
-      </div>
-    </div>
+      </ContactCardText>
+    </ContactCard>
   );
 };
