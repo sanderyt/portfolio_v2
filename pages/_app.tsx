24b6988de147a4ce4860e35632d74b20725fdc71
sanderyt <hallo@svanrijsoort.nl>
2020-11-17 21:57:35 +0000

    removed all the graphql stuff for now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,12 @@
 import { Theme } from "../styles/theme";
 import { GlobalStyle } from "../styles/globalStyle";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
-import { InMemoryCache } from "apollo-cache-inmemory";
-
-const fetch = require("node-fetch").default;
-
-const client = new ApolloClient({
-  uri:
-    "https://api-eu-central-1.graphcms.com/v2/ckhme4sza59pa01whgdli6slc/master",
-  fetch,
-  cache: new InMemoryCache()
-});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ApolloProvider client={client}>
-      <Theme>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </Theme>
-    </ApolloProvider>
+    <Theme>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </Theme>
   );
 }
 
