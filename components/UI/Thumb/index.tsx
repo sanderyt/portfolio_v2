7c2c15f1d58462d747b1e8211e99877430b88109
sanderyt <hallo@svanrijsoort.nl>
2020-11-17 13:34:48 +0000

    added a flexbox component

--- components/UI/Thumb/index.tsx
+++ components/UI/Thumb/index.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { FC } from "react";
 import Link from "next/link";
+import styled from "styled-components";
 
-export const Thumb = ({ name, tech, slug, id, thumb }) => {
+type ThumbProps = {
+  name: string;
+  tech: string;
+  slug: string;
+  id: number;
+  thumb: string;
+};
+
+const Container = styled.div`
+  height: auto;
+  width: 400px;
+  box-shadow: $box-shadow;
+  border-radius: $border-radius;
+  margin-top: 50px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+  animation: fadeIn 1s forwards;
+  transition: $transition;
+`;
+
+const Image = styled.div``;
+
+const Details = styled.div`
+  background-color: #f7f7f7;
+  border-top: 5px solid $primary-color;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  border-bottom-left-radius: $border-radius;
+  border-bottom-right-radius: $border-radius;
+  flex-direction: column;
+`;
+
+export const Thumb: FC<ThumbProps> = ({ name, tech, slug, id, thumb }) => {
   return (
     <Link href={`/project?id=${id}`}>
       <div className="thumb">
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,24 +3,33 @@ import React from "react";
 import { Layout } from "../components/UI/Layout";
 import { ContactCard } from "../components/UI/ContactCard";
 import { Container, Row, Col } from "styled-bootstrap-grid";
+import { FlexBox } from "react-styled-flex";
 import { Contactform } from "../components/Forms/Contactform";
 
 const Contact = () => {
   return (
     <Layout>
-      <Container>
+      <Container fluid>
         <Row>
-          <Col md={6} noGutter={true}>
-            <Contactform />
+          <Col lg={6} noGutter={true}>
+            <FlexBox center column>
+              <Contactform />
+            </FlexBox>
           </Col>
-          <Col md={6} noGutter={true}>
-            <ContactCard
-              text="+31 6 43 49 93 41"
-              type="Telephone"
-              icon="phone"
-            />
-            <ContactCard text="s.rijsoort@gmail.com" type="Email" icon="mail" />
-            <ContactCard text="Amsterdam, NL" type="City" icon="home" />
+          <Col lg={6} noGutter={true}>
+            <FlexBox center column>
+              <ContactCard
+                text="+31 6 43 49 93 41"
+                type="Telephone"
+                icon="phone"
+              />
+              <ContactCard
+                text="s.rijsoort@gmail.com"
+                type="Email"
+                icon="mail"
+              />
+              <ContactCard text="Amsterdam, NL" type="City" icon="home" />
+            </FlexBox>
           </Col>
         </Row>
       </Container>
