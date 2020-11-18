992a182f689577c657bc7fa41ba71849792dce93
sanderyt <hallo@svanrijsoort.nl>
2020-11-18 14:39:24 +0000

    removed a console log

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -7,23 +7,24 @@ import { Layout } from "../components/UI/Layout";
 import { Thumb } from "../components/UI/Thumb";
 import { Header } from "../components/UI/Header";
 import { Project } from "../lib/types";
+import { title } from "process";
 
 type ProjectsProps = {
   projects: Project[];
 };
 
 const Projects: FC<ProjectsProps> = ({ projects }) => {
-  console.log(projects, "projects");
   return (
     <Layout>
       <Header title="Projects" />
       {projects.map(project => {
+        const { title, id, slug, tech } = project;
         return (
           <Thumb
-            name={project.title}
-            id={project.id}
-            slug={project.slug}
-            tech={project.tech}
+            name={title}
+            id={id}
+            slug={slug}
+            tech={tech}
             thumb={project.projectImages.url as string}
           />
         );
