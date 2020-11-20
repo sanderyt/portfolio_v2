8c4fb73ca4eb6eddac952fc0244a6983c7348bd6
sanderyt <hallo@svanrijsoort.nl>
2020-11-20 14:15:25 +0000

    added moment js and colors of technologies

--- components/Pill/index.tsx
+++ components/Pill/index.tsx
@@ -23,7 +23,15 @@ export const Pill: FC<PillProps> = ({ children }) => {
     if (name === "Strapi") return "#8c4bff";
     if (name === "NextJS") return "#000";
     if (name === "TypeScript") return "#3178c6";
+    if (name === "VueJS") return "#42b983";
+    if (name === "Vuex") return "#42b983";
+    if (name === "PHP") return "#8892BF";
+    if (name === "WordPress") return "#0073aa";
+    if (name === "CSharp") return "#903ba7";
+    if (name === "Firebase") return "#ffcb31";
+    if (name === "Unity") return "#000";
     return "#808080";
   };
+
   return <StyledPill techColor={renderColor(children)}>{children}</StyledPill>;
 };
