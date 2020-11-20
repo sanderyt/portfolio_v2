db6cb9c0db2aea08885175758e69bb93dd317760
sanderyt <hallo@svanrijsoort.nl>
2020-11-20 15:36:33 +0000

    added some good styling for project detail page

--- components/UI/ProjectHeader/index.tsx
+++ components/UI/ProjectHeader/index.tsx
@@ -6,7 +6,7 @@ import { Col, Row, Container } from "styled-bootstrap-grid";
 import { FlexBox } from "react-styled-flex";
 
 import { Button } from "../Button";
-import { Pill } from "../../Pill";
+import { Pill } from "../Pill";
 
 type ProjectHeaderProps = {
   title: string;
@@ -24,6 +24,10 @@ const Title = styled.h1`
   color: ${props => props.theme.colors.primaryColor};
 `;
 
+const Detail = styled.span`
+  color: ${props => props.theme.colors.greyScales.text};
+`;
+
 export const ProjectHeader: FC<ProjectHeaderProps> = ({
   title,
   url,
@@ -35,16 +39,21 @@ export const ProjectHeader: FC<ProjectHeaderProps> = ({
     <StyledContainer fluid>
       <Row>
         <Col lg={6} md={12}>
-          <FlexBox column justifyContent="center" height="225px">
+          <FlexBox
+            column
+            justifyContent="center"
+            height="225px"
+            padding="0 0 0 48px"
+          >
             <Title>{title}</Title>
-            <span>Built with</span>
+            <Detail>Built with</Detail>
             <FlexBox wrap>
               {technologies.map(technology => {
                 return <Pill>{technology}</Pill>;
               })}
             </FlexBox>
             <FlexBox column>
-              <span>Date worked on</span>
+              <Detail>Project date</Detail>
               <span>
                 {moment(startDate).format("DD MMMM, YYYY")} -{" "}
                 {moment(endDate).format("DD MMMM, YYYY")}{" "}
---
+++ components/UI/ProjectImage/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type ProjectImageProps = {};
+
+export const ProjectImage: React.FC<ProjectImageProps> = ({}) => {
+  return <div></div>;
+};
