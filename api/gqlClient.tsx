a9b7d437bf86540a6fc93f0c2b0e5d74c214146b
sanderyt <hallo@svanrijsoort.nl>
2020-11-24 15:47:52 +0000

    hardcoded env var

--- api/gqlClient.tsx
+++ api/gqlClient.tsx
@@ -2,4 +2,7 @@ import { GraphQLClient } from "graphql-request";
 
 const endpoint = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
-export const client = new GraphQLClient(endpoint, { headers: {} });
+export const client = new GraphQLClient(
+  "https://api-eu-central-1.graphcms.com/v2/ckhme4sza59pa01whgdli6slc/master",
+  { headers: {} }
+);
