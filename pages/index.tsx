f4dd20250a7e914923fc0edc8ccc48ea4de81ed4
sanderyt <hallo@svanrijsoort.nl>
2020-12-01 23:26:08 +0000

    added next seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import { FC } from "react";
+import { NextSeo } from "next-seo";
 
 import { Layout } from "../components/UI/Layout";
 
 const Home: FC = () => (
   <Layout>
+    <NextSeo
+      title="React developer from Amsterdam"
+      description="My name is Sander van Rijsoort and have been a React developer for almost 2 years. Let's work together!"
+    />
     <h1>
       Hi, I am <span>Sander van Rijsoort.</span> I get in a flow by writing code.
     </h1>
