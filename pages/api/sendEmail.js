62bdbaf47bf69c1a22af58e72ea0302a6136e7e4
sanderyt <s.rijsoort@gmail.com>
2021-07-05 16:07:30 +0100

    deployed serverless email function for receiving emails

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -11,19 +11,37 @@ module.exports = async (req, res) => {
     return res.status(405).json("This method is not allowed");
   }
 
-  const recipients = [new Recipient("your@client.com", "Your Client")];
+  const { message, name, email } = req.body;
+
+  if (!message) {
+    return res.status(400).json("Missing message");
+  }
+
+  if (!name) {
+    return res.status(400).json("Missing name");
+  }
+
+  if (!message) {
+    return res.status(400).json("Missing message");
+  }
+
+  const recipients = [new Recipient("s.rijsoort@gmail.com", "Sander van Rijsoort")];
 
   const emailParams = new EmailParams()
-    .setFrom("info@domain.com")
-    .setFromName("Your Name")
+    .setFrom("info@reactdev.io")
+    .setFromName("Sander van Rijsoort")
     .setRecipients(recipients)
-    .setSubject("Subject")
-    .setHtml("This is the HTML content")
-    .setText("This is the text content");
+    .setSubject("A message from reactdev.io")
+    .setHtml(
+      `Dear Sander, You have received a new message from reactdev.io: ${message} <br /> Email is ${email}`
+    )
+    .setText(
+      `Dear Sander, You have received a new message from reactdev.io: ${message}. Email is ${email}`
+    );
 
   const response = await mailersend.send(emailParams);
 
-  if (response.status !== 200) {
+  if (response.status !== 202) {
     return res.json({ message: response.statusText });
   }
 
